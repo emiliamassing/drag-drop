172bdfc869965ec7d35572d738974e111cc861e5
Emilia <emiliamassings@gmail.com>
2023-10-02 19:10:48 +0200

    💄 ✨ Added result container, cards and styling

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,34 @@
 import './style/_general.scss';
 
+const numbers: string[] = ['4', '10', '5', '2', '3'];
+
+function createCards():void {
+  numbers.map((number) => {
+    const card: HTMLDivElement = document.createElement('div');
+    const heading: HTMLHeadingElement = document.createElement('h2');
+
+    card.className = 'card';
+    card.id = number;
+
+    heading.innerHTML = number;
+
+    card.appendChild(heading);
+    cardContainer.appendChild(card);
+  });
+};
+
 const app: HTMLDivElement = document.querySelector('#app') as HTMLDivElement;
 const main: HTMLElement = document.querySelector('.main') as HTMLElement;
 
 const resultContainer: HTMLDivElement = document.createElement('div') as HTMLDivElement;
+const cardContainer: HTMLDivElement = document.createElement('div') as HTMLDivElement;
 
 resultContainer.className = 'resultContainer';
+cardContainer.className = 'cardContainer';
 
 resultContainer.innerHTML = 'Result Container';
 
-main.appendChild(resultContainer);
+main.append(resultContainer, cardContainer);
 app.appendChild(main);
 
-
-
+createCards();
