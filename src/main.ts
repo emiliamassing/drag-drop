0047e23e68650588436898f6e23ee016897fda92
Emilia <emiliamassings@gmail.com>
2023-10-02 18:29:56 +0200

    🎨 Added normalize, variable scss files, removed unused code

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,16 @@
 import './style/_general.scss';
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const app: HTMLDivElement = document.querySelector('#app') as HTMLDivElement;
+const main: HTMLElement = document.querySelector('.main') as HTMLElement;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const resultContainer: HTMLDivElement = document.createElement('div') as HTMLDivElement;
+
+resultContainer.className = 'resultContainer';
+
+resultContainer.innerHTML = 'Result Container';
+
+main.appendChild(resultContainer);
+app.appendChild(main);
+
+
+
